d28eaeeb4a75ceea025d419f5a427d1ac37a95df
Marvin Lukas Wenuel <marvinl.wenzel@googlemail.com>
2018-11-01 15:42:02 +0100

    Example for UDP from 2018-11-01

---
+++ src/udp/Bob.java
@@ -0,0 +1,22 @@
+package udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Bob {
+
+    public static void main(String[] args) throws IOException {
+
+        DatagramSocket ds = new DatagramSocket(5252);
+        byte buf[] = new byte[2];
+        byte send[] = { 13, 18 };
+        DatagramPacket dp = new DatagramPacket(buf, 2);
+
+        ds.receive(dp);
+
+        DatagramPacket senddp = new DatagramPacket(send, 2,
+                dp.getAddress(), dp.getPort());
+        ds.send(senddp);
+    }
+}
